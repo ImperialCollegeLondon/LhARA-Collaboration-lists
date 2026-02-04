ecf18186b6086dbc73ad4e537725e97a7768d1b5
Ken Long <k.long@imperial.ac.uk>
2026-02-04 07:12:41 +0000

    Fix plane text author list

--- 04-LaTeX/getfromscratch.bash
+++ 04-LaTeX/getfromscratch.bash
@@ -3,3 +3,4 @@
 mv ../99-Scratch/01-AuthorList.tex           01-AuthorList/01-AuthorList.tex
 mv ../99-Scratch/01-AuthorsByInstitute.tex   01-AuthorsByInstitute/01-AuthorsByInstitute.tex
 mv ../99-Scratch/01-InstituteList.tex        01-InstituteList/01-InstituteList.tex
+mv ../99-Scratch/02-AuthorList.txt           LhARA-AuthorList-Tst.txt
